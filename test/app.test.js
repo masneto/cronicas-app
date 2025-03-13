549f68010073c1ca80aaee6e73f4805e9aa2d0a8
Mario Neto <masnetto1992@gmail.com>
2025-03-13 15:40:52 -0300

    Remove healthcheck

--- test/app.test.js
+++ test/app.test.js
@@ -1,4 +1,11 @@
 const request = require('supertest');
 const app = require('../src/app');
 
-// Remover o teste para o endpoint /health
+describe('GET /', () => {
+  it('should return index.html', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.headers['content-type']).toMatch(/html/);
+    expect(res.text).toContain('<!DOCTYPE html>');
+  });
+});
